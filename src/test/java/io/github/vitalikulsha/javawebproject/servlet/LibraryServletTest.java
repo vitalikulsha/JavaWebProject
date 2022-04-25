bb17e37aa77bf31624808a64b3043843ce2d85a8
vitalikulsha <kulsha_85@mail.ru>
2022-04-25 22:08:23 +0300

    add test methods in servletTest

--- src/test/java/io/github/vitalikulsha/javawebproject/servlet/LibraryServletTest.java
+++ src/test/java/io/github/vitalikulsha/javawebproject/servlet/LibraryServletTest.java
@@ -1,8 +1,17 @@
 package io.github.vitalikulsha.javawebproject.servlet;
 
+import io.github.vitalikulsha.javawebproject.DataBase;
+import io.github.vitalikulsha.javawebproject.book.entity.Book;
+import io.github.vitalikulsha.javawebproject.book.entity.BookDTO;
 import io.github.vitalikulsha.javawebproject.servlet.path.GuestPath;
+import io.github.vitalikulsha.javawebproject.servlet.path.UserPath;
+import io.github.vitalikulsha.javawebproject.user.entity.User;
+import io.github.vitalikulsha.javawebproject.user.entity.UserDTO;
 import io.github.vitalikulsha.javawebproject.util.constant.Page;
 import io.github.vitalikulsha.javawebproject.util.constant.RequestParameter;
+import io.github.vitalikulsha.javawebproject.util.constant.SessionAttribute;
+import io.github.vitalikulsha.javawebproject.util.dtoconverter.DTOConverter;
+import io.github.vitalikulsha.javawebproject.util.dtoconverter.DTOConverterFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import static org.mockito.Mockito.*;
 
@@ -40,7 +50,7 @@ public class LibraryServletTest {
     }
 
     @Test
-    public void testLoginCommandWithGetMethod() throws ServletException, IOException {
+    public void testLoginCommandGetMethod() throws ServletException, IOException {
         when(request.getServletPath()).thenReturn(GuestPath.LOGIN.getPath());
         when(request.getMethod()).thenReturn("GET");
         when(request.getSession()).thenReturn(httpSession);
@@ -52,7 +62,7 @@ public class LibraryServletTest {
     }
 
     @Test
-    public void testLoginCommandWithPostMethod() throws ServletException, IOException {
+    public void testLoginCommandPostMethod() throws ServletException, IOException {
         when(request.getServletPath()).thenReturn(GuestPath.LOGIN.getPath());
         when(request.getMethod()).thenReturn("POST");
         when(request.getSession()).thenReturn(httpSession);
@@ -93,6 +103,97 @@ public class LibraryServletTest {
         verify(request, times(2)).getSession();
         verify(response, times(1)).sendRedirect(any());
         verify(response).sendRedirect("/library/reader");
+    }
+
+    @Test
+    public void testLocaleCommand() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(GuestPath.LOCALE.getPath());
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getContextPath()).thenReturn("/library");
+        when(request.getParameter(RequestParameter.LOCALE)).thenReturn("en");
+        when(httpSession.getAttribute(SessionAttribute.URL)).thenReturn("/reader");
+        servlet.doGet(request, response);
+        verify(request, times(1)).getSession();
+        verify(response, times(1)).sendRedirect(any());
+        verify(response).sendRedirect("/library/reader");
+        verify(response, never()).sendRedirect("/library/login");
+    }
+
+    @Test
+    public void testReaderCommand() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(UserPath.READER.getPath());
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getRequestDispatcher(Page.READER)).thenReturn(dispatcher);
+        servlet.doGet(request, response);
+        verify(request).getSession();
+        verify(request, times(1)).getRequestDispatcher("/WEB-INF/view/reader/reader.jsp");
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void testBookSearchCommand() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(UserPath.BOOK_SEARCH.getPath());
+        when(request.getRequestDispatcher(Page.BOOK_SEARCH)).thenReturn(dispatcher);
+        servlet.doGet(request, response);
+        verify(request, never()).getSession();
+        verify(request, times(1)).getRequestDispatcher("/WEB-INF/view/reader/book-search.jsp");
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void testCatalogCommand() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(UserPath.CATALOG.getPath());
+        when(request.getRequestDispatcher(Page.CATALOG)).thenReturn(dispatcher);
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getContextPath()).thenReturn("/library");
+        when(request.getParameterMap()).thenReturn(new HashMap<>() {{
+            this.put("page", new String[]{"1"});
+            this.put("bookTitle", new String[]{"и"});
+        }});
+        servlet.doGet(request, response);
+        verify(request).getSession();
+        verify(request).getRequestDispatcher("/WEB-INF/view/reader/catalog.jsp");
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void testOrderCommandGetMethod() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(UserPath.ORDER.getPath());
+        when(request.getRequestDispatcher(Page.ORDER)).thenReturn(dispatcher);
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getParameter(RequestParameter.BOOK_ID)).thenReturn("10100");
+        servlet.doGet(request, response);
+        verify(request).getSession();
+        verify(request).getRequestDispatcher("/WEB-INF/view/reader/order.jsp");
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void testOrderCommandPostMethod() throws ServletException, IOException {
+        when(request.getServletPath()).thenReturn(UserPath.ORDER.getPath());
+        when(request.getSession()).thenReturn(httpSession);
+        when(request.getContextPath()).thenReturn("/library");
+        when(request.getMethod()).thenReturn("POST");
+        when(request.getParameter(RequestParameter.RESERVE_STATUS)).thenReturn("LOANED");
+        DTOConverter<BookDTO, Book> bookConverter = DTOConverterFactory.instance().bookDtoConverter();
+        BookDTO bookDTO = DataBase.BOOK_TABLE.stream()
+                .filter(b -> b.getId() == 12300)
+                .findFirst()
+                .map(bookConverter::toDto)
+                .get();
+        when(httpSession.getAttribute(SessionAttribute.BOOK)).thenReturn(bookDTO);
+        DTOConverter<UserDTO, User> userConverter = DTOConverterFactory.instance().userDtoConverter();
+        UserDTO userDTO = DataBase.USER_TABLE.stream()
+                .filter(u -> u.getId() == 3)
+                .findFirst()
+                .map(userConverter::toDto)
+                .get();
+        when(httpSession.getAttribute(SessionAttribute.USER)).thenReturn(userDTO);
+        servlet.doPost(request, response);
+        verify(request, times(1)).getSession();
+        verify(response, times(1)).sendRedirect(any());
+        verify(response).sendRedirect("/library/reader/reader-orders");
     }
 
     @After
